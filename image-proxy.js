5af33331f0e129cc8efd59b45ffc87b8d904b091
Chetan Bansal <chetan1@gmail.com>
2012-05-31 00:32:27 +0530

    Effect chaining & grayscale

--- image-proxy.js
+++ image-proxy.js
@@ -13,58 +13,54 @@ var image_proxy = function(imagePath){
     server.get('/image', function(req, res, next){
 
         var url = req.param('url');
-        var action = req.param('action');
 
         // Add support for remote files by downloading the remote file to the images directory
         fs.stat(url, function(err, stat) {
 
             if (!err) {
 
+                var outputImage = gm(url);
                 var imageExt = path.extname(url);
 
-                switch(action)
+                if(req.param('resize') == 1)
                 {
-                    case 'resize':
+                    var newWidth = req.param('width');
+                    var newHeight = req.param('height');
+
+                    if(newHeight == null && newWidth == null)
+                        throw("Atleast one parameter from height or width should be defined.");
+                    else
                     {
-                        var newWidth = req.param('width');
-                        var newHeight = req.param('height');
-
-                        if(newHeight == null && newWidth == null)
-                            throw("Atleast one parameter from height or width should be defined.");
-                        else
-                        {
-                            // resize & render
-                            gm(url).resize(newWidth, newHeight).stream( function(err, stdout, stderr) {
-
-                                res.setHeader('Content-type', 'image/jpeg');
-
-                                stdout.on('data', function(chunk) {
-                                    res.write(chunk);
-                                });
-                                stdout.on('end', function() {
-                                    res.end();
-                                });
-
-                            });
-                        }
+                        outputImage = outputImage.resize(newWidth, newHeight);
                     }
-                    break;
-                    case 'grayscale':
-                        //                            Grayscale
-                        //                            gm("img.png").type('grayscale');
-                        break;
-                    default:
-                        throw "Please define a valid action!";
                 }
+
+                if(req.param('grayscale') == 1)
+                {
+                    outputImage = outputImage.type('grayscale');
+                }
+
+                outputImage.stream(renderImage);
 
             }else {
                 throw err;
             }
         });
+
+        function renderImage(err, stdout, stderr) {
+
+            res.setHeader('Content-type', 'image/jpeg');
+
+            stdout.on('data', function(chunk) {
+                res.write(chunk);
+            });
+            stdout.on('end', function() {
+                res.end();
+            });
+        }
     });
 
     return server;
 };
 
-//exports.imagePath = imagePath;
 exports.server = image_proxy;
