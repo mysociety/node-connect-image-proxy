dcc5baab7305dffbd74c9bff4592f4b36982d6d9
Chetan Bansal <chetan1@gmail.com>
2012-05-30 20:52:28 +0530

    Direct streaming and temp path configuration

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
  * Module dependencies.
  */
 
-var image_proxy = require('./image-proxy');
-  
-image_proxy.server.listen(3000, function(){
-    console.log("Express server listening on port %d in %s mode", image_proxy.server.address().port, image_proxy.server.settings.env);
+var image_proxy = require('./image-proxy1');
+
+image_proxy.server().listen(3000, function(){
+    console.log("Express server listening");
 });
--- image-proxy.js
+++ image-proxy.js
@@ -5,72 +5,67 @@ gm                = require('gm');
 
 var server = express.createServer();
 
-server.get('/image', function(req, res, next){
+var image_proxy = function(imagePath){
 
-    var url = req.param('url');
-    var action = req.param('action');
+    if(imagePath == undefined)
+        imagePath = "\img\\";
 
-    // Add support for remote files by downloading the remote file to the images directory
-    fs.stat(url, function(err, stat) {
+    server.get('/image', function(req, res, next){
 
-        if (!err) {
+        var url = req.param('url');
+        var action = req.param('action');
 
-            var imageType = path.extname(url);
-            var newFileName = "\images\\" + Math.floor(Math.random()*1000001) + imageType;
+        // Add support for remote files by downloading the remote file to the images directory
+        fs.stat(url, function(err, stat) {
 
-            switch(action)
-            {
-                case 'resize':
+            if (!err) {
+
+                var imageExt = path.extname(url);
+
+                switch(action)
                 {
-                    var newWidth = req.param('width');
-                    var newHeight = req.param('height');
-
-                    if(newHeight == null && newWidth == null)
-                        throw("Atleast one parameter from height or width should be defined.");
-                    else
+                    case 'resize':
                     {
-                        // resize & render
-                        gm(url)
-                        .resize(newWidth, newHeight)
-                        .write(newFileName, function (err) {
-                            if (!err)
-                            {
-                                renderNewImage(res);
-                            }
-                        });
+                        var newWidth = req.param('width');
+                        var newHeight = req.param('height');
 
+                        if(newHeight == null && newWidth == null)
+                            throw("Atleast one parameter from height or width should be defined.");
+                        else
+                        {
+                            // resize & render
+
+                            gm(url).resize(newWidth, newHeight).stream( function(err, stdout, stderr) {
+
+                                res.setHeader('Content-type', 'image/jpeg');
+
+                                stdout.on('data', function(chunk) {
+                                    res.write(chunk);
+                                });
+                                stdout.on('end', function() {
+                                    res.end();
+                                });
+
+                            });
+                        }
                     }
-                }
-                break;
-                case 'grayscale':
-                    //                            Grayscale
-                    //                            gm("img.png").type('grayscale');
                     break;
-                default:
-                    throw "Please define a valid action!";
+                    case 'grayscale':
+                        //                            Grayscale
+                        //                            gm("img.png").type('grayscale');
+                        break;
+                    default:
+                        throw "Please define a valid action!";
+                }
+
+            }else {
+                res.send(err);
             }
-
-            function renderNewImage(res)
-            {
-                // Set the appropriate headers
-                res.setHeader('Content-type', 'image/jpeg');
-
-                var filestream = fs.createReadStream(newFileName);
-                filestream.on('data', function(chunk) {
-                    // Stream the new file
-                    res.write(chunk);
-                });
-                filestream.on('end', function() {
-                    // Delete the temp file
-//                    fs.unlinkSync(newFileName);  // Giving error 
-                    res.end();                    
-                });
-            }
-
-        }else {
-            res.send(err);
-        }
+        });
     });
-});
 
-exports.server = server;
+    return server;
+};
+
+//exports.imagePath = imagePath;
+exports.server = image_proxy;
