f40f983ef0fb2e31dd92a0c346d5398474667ee1
Chetan Bansal <chetan1@gmail.com>
2012-06-01 16:36:57 +0530

    Added view configuration

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ var image_proxy = require('./image-proxy');
 var express = require('express');
 var app = express.createServer();
 
+app.configure(function(){
+  app.set('view engine', 'jade');
+  app.set('views', __dirname + '/views');
+  app.set('view options', { layout: false, pretty: true});
+});
+
+
 app.get('/image', image_proxy.run);
 
 app.get('/test', function(req, res){
-    res.render('test.jade', { layout: false });
+    res.render('test.jade');
 });
 
 app.listen(3000);
